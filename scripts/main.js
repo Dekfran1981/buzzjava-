d4d5a4c5e44c1ce06b7733063a47bc6a2174afbb
Dekfran1981 <patriciocb001@outlook.com>
2023-09-13 11:12:28 -0300

    Mensaje

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+let nombreCurso;
+
+nombreCurso = "Javascript";
+console.log("--> El nombre del curso al que le damos la bienvenida es " + nombreCurso)
+
+const NOMBRE_PROFESOR = "Saul"
+
+let nombreEstudiante = "Pepe GRillo "
+const DIAS_DE_LA_SEMANA = 7;
+
+let comision = " 47600"
+
+console.log("--> Esto es el curso de " + nombreCurso + " - " + comision );
+
+let numeroUno = 5
+let numeroDos = 10
+let resultado = numero + numeroDos;
+console.log("--> Resultado " + resultado );
+
+nombreCurso = prompt("Que curso estas dictando hoy ?");
+nombreEstudiante = prompt ( " dime el nombre de un estudiante ");
+console.log("--> Ingesaste un valor de el estudiante con nombre : " + nombreEstudiante);
+
+alert( " nombreEstudiante + " , nombreCurso); 
